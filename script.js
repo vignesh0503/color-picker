c56e7b4a9843edd1c97d94a372fef4eb3358c0c2
vignesh0503 <vignesh.s170503@gmail.com>
2024-03-22 11:21:19 +0530

    color picker initial

---
+++ script.js
@@ -0,0 +1,23 @@
+function button_1() {
+  document.getElementById("colorPickerContainer").style.backgroundColor =
+    "#e0e0e0";
+  document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+}
+
+function button_2() {
+  document.getElementById("colorPickerContainer").style.backgroundColor =
+    "#6fcf97";
+  document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+}
+
+function button_3() {
+  document.getElementById("colorPickerContainer").style.backgroundColor =
+    "#56ccf2";
+  document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+}
+
+function button_4() {
+  document.getElementById("colorPickerContainer").style.backgroundColor =
+    "#bb6bd9";
+  document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+}
